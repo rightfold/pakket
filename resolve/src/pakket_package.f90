825c9d091e8319d458c9862d2a9d88018d7e390d
rightfold <rightfold@gmail.com>
2018-05-24 21:32:05 +0200

    [resolve] Add package version bounds and package atoms

---
+++ resolve/src/pakket_package.f90
@@ -0,0 +1,38 @@
+MODULE pakket_package
+
+    IMPLICIT NONE
+
+    ! A pair of package version bounds limits which package versions can be
+    ! installed.
+    TYPE bounds_t
+        INTEGER :: lower
+        INTEGER :: upper
+    END TYPE
+
+    ! A package atom pairs a package with a pair of package version bounds.
+    TYPE atom_t
+        INTEGER :: package
+        TYPE(bounds_t) :: bounds
+    END TYPE
+
+CONTAINS
+
+    ! A pair of package version bounds is invalid if its upper bound is lower
+    ! than its lower bound.
+    FUNCTION bounds_valid(a)
+        TYPE(bounds_t), INTENT(IN) :: a
+        LOGICAL :: bounds_valid
+        bounds_valid = a%lower .LT. a%upper
+    END FUNCTION
+
+    ! What is the intersection of these two pairs of package version bounds? In
+    ! case there is no intersection, this function returns an invalid pair of
+    ! package version bounds.
+    FUNCTION bounds_intersect(a, b)
+        TYPE(bounds_t), INTENT(IN) :: a, b
+        TYPE(bounds_t) :: bounds_intersect
+        bounds_intersect%lower = MAX(a%lower, b%lower)
+        bounds_intersect%upper = MIN(a%upper, b%upper)
+    END FUNCTION
+
+END MODULE
